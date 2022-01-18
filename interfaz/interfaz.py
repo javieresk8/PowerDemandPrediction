977602bf053920fd88de2afe5afb1f8ee3c554cc
Anderson <anderson.cardenas.r@gmail.com>
2022-01-17 19:13:32 -0500

    Interfaz elementos base

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -1,5 +1,5 @@
 from logging import root
-from tkinter import Canvas, Tk,Frame ,LAST, ttk
+from tkinter import GROOVE, SUNKEN, Canvas, Tk,Frame ,LAST, ttk, Button
 from math import cos, sin, radians, pi
 import random
 from typing import Text
@@ -7,8 +7,22 @@ from typing import Text
 from tkinter import PhotoImage
 from PIL import Image,ImageTk
 
+# Colores
 global colorBlanco
 colorBlanco = '#F7F7F7'
+global colorNegro
+colorNegro = '#090909'
+global colorAzul
+colorAzul = '#0853B3'
+
+#Dimensiones
+global ancho_ventana
+ancho_ventana = 950
+global alto_ventana
+alto_ventana = 650
+global heightSubFrames
+heightSubFrames = 450
+
  
 class Modelos: 
 
@@ -44,35 +58,63 @@ def cargarImg(root, file, size: tuple, place: tuple):
 
 def mainInterfaz():
 
+    def mostrarDatosHistoricos():
+        pass
+    def ocultarDatosHistoricos():
+        frameDatosHsitoricos.place_forget() 
+
     # Cracción Root y configuraciones iniciales
     ventana = Tk()
-    ancho_ventana = 950
-    alto_ventana = 600
     posicion = str(ancho_ventana) + "x" + str(alto_ventana)
     ventana.geometry(posicion)
     ventana.resizable(0,0)
     ventana.minsize(width=ancho_ventana, height=alto_ventana)
     ventana.title('Predicción Demanda')
     ventana.config(bg=colorBlanco)
+    ventana.iconbitmap('img/icono.ico')
     center(ventana)
 
     #Frame -> Contenedor
-    frame = Frame(ventana, width = ancho_ventana,  height=alto_ventana, bg= colorBlanco, relief ='sunken')
-    frame.grid(column=0,row=0)
+    frameGlobal = Frame(ventana, width = ancho_ventana,  height=alto_ventana, bg= colorBlanco, relief ='sunken')
+    frameGlobal.grid(column=0,row=0)
 
-    # Encabezado 
-    cargarImg(ventana, 'logo1.png',(100,100),(30,20))
-    cargarImg(ventana, 'logo2.png',(100,100),(800,20))
+    #  ---------------------------------------  Encabezado  ------------------------------------------------- #
+    cargarImg(ventana, 'img/logo1.png',(100,100),(30,20))
+    cargarImg(ventana, 'img/logo2.png',(100,100),(800,20))
 
-    label1 = ttk.Label(frame, text="UNIVERSIDAD TÉCNICA DE COTOPAXI", background=colorBlanco, font=("Courie",14,'bold'))
-    label1.place(x=280, y=30)
+    label1 = ttk.Label(frameGlobal, text="UNIVERSIDAD TÉCNICA DE COTOPAXI", background=colorBlanco, font=("Courie",14,'bold'))
+    label1.place(x=280, y=10)
 
-    label2 = ttk.Label(frame, text="INGENIERÍA ELÉCTRICA", background=colorBlanco, font=("Courie",14,'bold'))
-    label2.place(x=350, y=70)
+    label2 = ttk.Label(frameGlobal, text="INGENIERÍA ELÉCTRICA", background=colorBlanco, font=("Courie",14,'bold'))
+    label2.place(x=350, y=40)
 
-    # Canvas Parqueaderos
-    #canvasParqueadero = Canvas(frame, bg= '#50A3D3', width = 300, height =320, bd =0, highlightthickness=2, highlightbackground="white")
-    #canvasParqueadero.place(x=30,y=50)
+    canvasAutores = Canvas(frameGlobal, bg= colorBlanco, width = 350, height =50, bd =0, highlightthickness=2, highlightbackground=colorNegro)
+    canvasAutores.place(x=300,y=75)
+
+    labelAutores = ttk.Label(canvasAutores, text="Autores:\n-Autor1\n-Autor2", background=colorBlanco, font=("Courie",8,'bold'))
+    labelAutores.place(x=5, y=5)
+
+    # Botones encabezado
+    btnDatosHistoricos = Button(frameGlobal,text= 'Datos Históricos',font=("Courie",10,'bold'),command=mostrarDatosHistoricos, width=20, height=2, bd= 2,bg=colorBlanco, relief=GROOVE, highlightbackground=colorNegro)
+    btnDatosHistoricos.place(x=50,y=140)
+
+    btnEntrenamiento = Button(frameGlobal,text= 'Entrenamiento Red \nNeuronal',font=("Courie",10,'bold'),command=mostrarDatosHistoricos, width=20, height=2, bd= 2,bg=colorBlanco, relief=GROOVE, highlightbackground=colorNegro)
+    btnEntrenamiento.place(x=270,y=140)
+
+    btnPrediccion = Button(frameGlobal,text= 'Predicción de\nla demanda',font=("Courie",10,'bold'),command=mostrarDatosHistoricos, width=20, height=2, bd= 2,bg=colorBlanco, relief=GROOVE, highlightbackground=colorNegro)
+    btnPrediccion.place(x=490,y=140)
+
+    btnSalir = Button(frameGlobal,text= 'Salir',font=("Courie",10,'bold'),command=ventana.destroy, width=20, height=2, bd= 2,bg=colorBlanco, relief=GROOVE, highlightbackground=colorNegro)
+    btnSalir.place(x=710,y=140)
+
+    # ------------------------------------------------------------------------------------------------------- #
+
+    #  ----------------------------  Frame de Datos Históricos -----------------------------------------------#
+    global frameDatosHsitoricos
+    frameDatosHsitoricos = Frame(frameGlobal, width = ancho_ventana,  height=heightSubFrames, bg= colorAzul, relief ='sunken')
+    frameDatosHsitoricos.place(x=0,y=200)
+
+    # --------------------------------------------------------------------------------------------------------------#
 
     # Canvas Controles
     #canvasInfo = Canvas(frame, bg= '#50A3D3', width = 150, height =400, bd =0, highlightthickness=2, highlightbackground="white")
@@ -93,8 +135,6 @@ def mainInterfaz():
     #labelBault2 = ttk.Label(frame,width=8,text='Bault', background='#50A3D3', font=("Courie",8))
     #labelBault2.place(x=560, y=190)
                    
-  
-
     ventana.mainloop()
 
 mainInterfaz()
