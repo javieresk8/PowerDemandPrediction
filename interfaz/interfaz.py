93c25d3524fbe82e7ef657c8bc5e7a33fc70cd22
Anderson <anderson.cardenas.r@gmail.com>
2022-01-21 23:26:41 -0500

    Controles frame datos históricos

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -35,6 +35,15 @@ heightSubFrames = 450
 global posicionFrames
 posicionFrames = (0,200)
 
+# Listas de datos
+global listaAnos
+listaAnos = [2015,2016,2017,2018,2019,2020,2021]
+global listaMeses
+listaMeses = [1,2,3,4,5,6,7,8,9,10,11,12]
+global listaDias
+listaDias = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+global listaHoras
+listaHoras = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 
  
 class Modelos: 
@@ -42,6 +51,8 @@ class Modelos:
     def __init__(self,modelo, optimizador):
         self.modelo = modelo
         self.optimizardor = optimizador
+
+
 
 def center(win): 
     """ centers a tkinter window :param win: the main window or Toplevel window to center """ 
@@ -179,12 +190,30 @@ class Interfaz:
         btnSalir.place(x=710,y=140)
 
 
-        # --------------- Elementos Frame Datos históricos ---------------------------- #
+#  1.  --------------- Elementos Frame Datos históricos ---------------------------- #
 
+        # 1. Canvas de controles
         canvasDatosHistoricos = Canvas(self.frameDatosHistoricos, bg= colorBlanco,
-         width = 300, height =300, bd =0, highlightthickness=2, highlightbackground="white")
+         width = 250, height =240, bd =0, highlightthickness=2, highlightbackground="white")
         canvasDatosHistoricos.place(x=15,y=10)
-     
+
+        # 2. Botones de control
+        
+        btnvisualizarDatos = Button(self.frameDatosHistoricos,text= 'Visualizar Datos',
+            font=("Courie",10,'bold'),command=self.nada, width=31, height=2, 
+            bd= 2,bg=colorBlanco, relief=GROOVE, highlightbackground=colorAzul)
+        btnvisualizarDatos.place(x=15,y=265)
+
+        btnGraficar = Button(self.frameDatosHistoricos,text= 'Graficar',
+            font=("Courie",10,'bold'),command=self.nada, width=31, height=2, 
+            bd= 2,bg=colorBlanco, relief=GROOVE, highlightbackground=colorAzul)
+        btnGraficar.place(x=15,y=315)
+
+        btnExportar = Button(self.frameDatosHistoricos,text= 'Exportar gráfico',
+            font=("Courie",10,'bold'),command=self.nada, width=31, height=2, 
+            bd= 2,bg=colorBlanco, relief=GROOVE, highlightbackground=colorAzul)
+        btnExportar.place(x=15,y=365)
+
 
         def seleccionarRadioButton():
             print('Seleccionaste: ',opcion.get())        
@@ -193,66 +222,99 @@ class Interfaz:
         global opcion
         opcion = StringVar()
         
+        # --- Sección de radio buttons
 
         label0 = ttk.Label(canvasDatosHistoricos,width=22,text='Datos', background=colorBlanco, font=("Courie",10,'bold'))
-        label0.place(x=35, y=15)
+        label0.place(x=100, y=15)
         
         radioTotal = Radiobutton(canvasDatosHistoricos, text="Total", variable=opcion, 
             value='Total', command=seleccionarRadioButton, tristatevalue=0)
-        radioTotal.place(x=20,y=40)
+        radioTotal.place(x=80,y=40)
 
         radioParcial = Radiobutton(canvasDatosHistoricos, text="Parcial", variable=opcion, 
             value='Parcial', command=seleccionarRadioButton, tristatevalue=0)
-        radioParcial.place(x=20,y=60)
+        radioParcial.place(x=80,y=60)
 
-        label1 = ttk.Label(canvasDatosHistoricos,width=6,text='Desde', background=colorBlanco, font=("Courie",10))
-        label1.place(x=20, y=100)
+        # -- Encabezados para seleccionar fecha
+
+        label0 = ttk.Label(canvasDatosHistoricos,width=6,text='Desde', background=colorBlanco, font=("Courie",10, 'bold'))
+        label0.place(x=20, y=90)
         
-        label2 = ttk.Label(canvasDatosHistoricos,width=6,text='Hasta', background=colorBlanco, font=("Courie",10))
-        label2.place(x=150, y=100)
+        label2 = ttk.Label(canvasDatosHistoricos,width=6,text='Hasta', background=colorBlanco, font=("Courie",10,'bold'))
+        label2.place(x=150, y=90)
+
+        # --- Parte izquierda desde
 
         label3 = ttk.Label(canvasDatosHistoricos,width=6,text='Año', background=colorBlanco, font=("Courie",10))
         label3.place(x=5, y=120)
 
-        combo = ttk.Combobox(canvasDatosHistoricos,values=[2018,2019,2020], width=5)
-        combo.place(x=45,y=120)
+        combo1 = ttk.Combobox(canvasDatosHistoricos,values=listaAnos, width=5)
+        combo1.place(x=45,y=120)
 
         label4 = ttk.Label(canvasDatosHistoricos,width=6,text='Mes', background=colorBlanco, font=("Courie",10))
         label4.place(x=5, y=150)
 
-        combo2 = ttk.Combobox(canvasDatosHistoricos,values=[1,2,3,4,5,6,7,8,9,10,11,12], width=5)
+        combo2 = ttk.Combobox(canvasDatosHistoricos,values=listaMeses, width=5)
         combo2.place(x=45,y=150)
        
         label5 = ttk.Label(canvasDatosHistoricos,width=6,text='Día', background=colorBlanco, font=("Courie",10))
         label5.place(x=5, y=180)
 
         combo3 = ttk.Combobox(canvasDatosHistoricos,
-            values=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+            values=listaDias,
             width=5)
         combo3.place(x=45,y=180)
        
+        label6 = ttk.Label(canvasDatosHistoricos,width=6,text='Hora', background=colorBlanco, font=("Courie",10))
+        label6.place(x=5, y=210)
+
+        combo4 = ttk.Combobox(canvasDatosHistoricos,
+            values=listaHoras,
+            width=5)
+        combo4.place(x=45,y=210)
+
+
+        # Parte derecha - Hasta
+        
+        label7 = ttk.Label(canvasDatosHistoricos,width=6,text='Año', background=colorBlanco, font=("Courie",10))
+        label7.place(x=125, y=120)
+
+        combo5 = ttk.Combobox(canvasDatosHistoricos,values=listaAnos, width=5)
+        combo5.place(x=165,y=120)
+
+        label4 = ttk.Label(canvasDatosHistoricos,width=6,text='Mes', background=colorBlanco, font=("Courie",10))
+        label4.place(x=125, y=150)
+
+        combo6 = ttk.Combobox(canvasDatosHistoricos,values=listaMeses, width=5)
+        combo6.place(x=165,y=150)
+       
+        label5 = ttk.Label(canvasDatosHistoricos,width=6,text='Día', background=colorBlanco, font=("Courie",10))
+        label5.place(x=125, y=180)
+
+        combo7 = ttk.Combobox(canvasDatosHistoricos,
+            values=listaDias,
+            width=5)
+        combo7.place(x=165,y=180)
+       
+        label6 = ttk.Label(canvasDatosHistoricos,width=6,text='Hora', background=colorBlanco, font=("Courie",10))
+        label6.place(x=125, y=210)
+
+        combo8 = ttk.Combobox(canvasDatosHistoricos,
+            values=listaHoras,
+            width=5)
+        combo8.place(x=165,y=210)
+
+
+#  2.  --------------- Elementos Frame Entrenamiento Red Neuronal ---------------------------- #
+
+        # 1. Canvas de controles
+        canvasEntrenamiento = Canvas(self.frameDatosHistoricos, bg= colorBlanco,
+         width = 250, height =240, bd =0, highlightthickness=2, highlightbackground="white")
+        canvasDatosHistoricos.place(x=15,y=10)       
        
 
-        # ------------------------------------------------------------------------------------------------------- #                
-        # Canvas Controles
-        #canvasInfo = Canvas(frame, bg= '#50A3D3', width = 150, height =400, bd =0, highlightthickness=2, highlightbackground="white")
-        #canvasInfo.place(x=550,y=50)
+        
 
-        #combo = ttk.Combobox(state="readonly", values=['COM 0','COM 1'])
-        #combo.place(x=560,y=110)
-
-        #label0 = ttk.Label(frame,width=22,text='ZigBee Communication Port', background='#50A3D3', font=("Courie",8))
-        #label0.place(x=560, y=80)
-
-        #labelSalida = ttk.Label(frame,width=22,text='', background='white', font=("Courie",8))
-        #labelSalida.place(x=560, y=160)
-
-        #labelBault1 = ttk.Label(frame,width=8,text='', background='white', font=("Courie",8))
-        #labelBault1.place(x=620, y=190)
-
-        #labelBault2 = ttk.Label(frame,width=8,text='Bault', background='#50A3D3', font=("Courie",8))
-        #labelBault2.place(x=560, y=190)   
-        # 
         ventana.mainloop()          
 
 interfaz = Interfaz()
